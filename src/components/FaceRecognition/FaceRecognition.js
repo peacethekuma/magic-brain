2e521aef43df5e93f68c765315b2e86d5629c5c2
peacethekuma <peacethekuma@gmail.com>
2018-11-10 10:14:38 +0800

    Build Login/Register Form and route functionality

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecignition.css'
 
-const FaceRecignition =({imageUrl})=>{
-  return(
+const FaceRecignition = ({ imageUrl,box }) => {
+  return (
     <div className='center mt3'>
       <div className='absolute mt2'>
-      <img alt=''src={imageUrl} max-width='500px' height='auto'/>
+        <img id='inputImage' alt='' src={imageUrl} max-width='500px' height='auto' />
+        <div className='bounding-box' style={{top:box.topRow,right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>
       </div>
     </div>
   )
